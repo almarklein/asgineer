64ec4d5a7bb17aae6e2bd20b37abef23f837fba1
Almar Klein <almar.klein@gmail.com>
2018-09-19 22:39:52 +0200

    Move Request class up in the code (easier read)

--- asgish.py
+++ asgish.py
@@ -25,142 +25,6 @@ def handler2asgi(handler):
     return Application
 
 
-class BaseApplication:
-    """ Base ASGI application class.
-    """
-
-    _handler = None
-
-    def __init__(self, scope):
-        self._scope = scope
-
-    async def __call__(self, receive, send):
-        request = Request(self._scope, receive)
-
-        # === Call handler to get the result
-        try:
-
-            result = await self._handler(request)
-
-        except Exception as err:
-            # Error in the handler
-            errer_text = "Error in request handler: " + str(err)
-            await send({"type": "http.response.start", "status": 500, "headers": []})
-            await send({"type": "http.response.body", "body": errer_text.encode()})
-            raise err
-
-        # === Process the handler output
-        try:
-
-            # Get status, headers and body from the result
-            if isinstance(result, tuple):
-                if len(result) == 3:
-                    status, headers, body = result
-                elif len(result) == 2 and isinstance(result[0], (set, dict)):
-                    # Also go this path on set to get a better error below
-                    status = 200
-                    headers, body = result
-                elif len(result) == 2:
-                    headers = {}
-                    status, body = result
-                elif len(result) == 1:
-                    status, headers, body = 200, {}, result[0]
-                else:
-                    raise ValueError(f"Handler returned {len(result)}-tuple.")
-            else:
-                status, headers, body = 200, {}, result
-
-            # Validate status and headers
-            assert isinstance(
-                status, int
-            ), f"Status code must be an int, not {type(status)}"
-            assert isinstance(
-                headers, dict
-            ), f"Headers must be a dict, not {type(headers)}"
-
-            # Convert the body
-            if isinstance(body, bytes):
-                pass
-            elif isinstance(body, str):
-                if body.startswith(("<!DOCTYPE ", "<html>")):
-                    headers.setdefault("content-type", "text/html")
-                else:
-                    headers.setdefault("content-type", "text/plain")
-                body = body.encode()
-            elif isinstance(body, dict):
-                try:
-                    body = json.dumps(body).encode()
-                except Exception as err:
-                    raise ValueError(f"Could not JSON encode body: {err}")
-                headers.setdefault("content-type", "application/json")
-            elif inspect.isasyncgen(body):
-                pass
-            else:
-                if inspect.isgenerator(body):
-                    raise ValueError(
-                        f"Body cannot be a regular generator, use an async generator."
-                    )
-                else:
-                    raise ValueError(f"Body cannot be {type(body)}.")
-
-            # Convert and further validate headers
-            if isinstance(body, bytes):
-                headers.setdefault("content-length", str(len(body)))
-            try:
-                rawheaders = [(k.encode(), v.encode()) for k, v in headers.items()]
-            except Exception:
-                raise ValueError("Header keys and values must all be strings.")
-
-        except Exception as err:
-            # Error in hanlding handler output
-            errer_text = "Error in processing handler output: " + str(err)
-            await send({"type": "http.response.start", "status": 500, "headers": []})
-            await send({"type": "http.response.body", "body": errer_text.encode()})
-            raise err
-
-        # === Send response
-        start = {"type": "http.response.start", "status": status, "headers": rawheaders}
-        if isinstance(body, bytes):
-            # The easy way; body as one message, not much error catching we can do here.
-            await send(start)
-            if isinstance(body, bytes):
-                await send({"type": "http.response.body", "body": body})
-        else:
-            # Chunked response, stuff can go wrong in the middle
-            start_is_sent = False
-            try:
-                async for chunk in body:
-                    if isinstance(chunk, str):
-                        chunk = chunk.encode()
-                    assert isinstance(
-                        chunk, bytes
-                    ), f"Body chunk must be str or bytes, not {type(chunk)}"
-                    if not start_is_sent:
-                        start_is_sent = True
-                        await send(start)
-                    await send(
-                        {"type": "http.response.body", "body": chunk, "more_body": True}
-                    )
-                await send(
-                    {"type": "http.response.body", "body": b"", "more_body": False}
-                )
-
-            except Exception as err:
-                if not start_is_sent:
-                    errer_text = "Error in chunked response: " + str(err)
-                    await send(
-                        {"type": "http.response.start", "status": 500, "headers": []}
-                    )
-                    await send(
-                        {"type": "http.response.body", "body": errer_text.encode()}
-                    )
-                else:  # end-of-body has also not been send
-                    await send(
-                        {"type": "http.response.body", "body": b"", "more_body": False}
-                    )
-                raise err
-
-
 class Request:
     """ Representation of an HTTP request. An object of this class is
     passed to the request handler.
@@ -300,3 +164,139 @@ class Request:
         """
         body = await self.get_body(limit)
         return json.loads(body.decode())
+
+
+class BaseApplication:
+    """ Base ASGI application class.
+    """
+
+    _handler = None
+
+    def __init__(self, scope):
+        self._scope = scope
+
+    async def __call__(self, receive, send):
+        request = Request(self._scope, receive)
+
+        # === Call handler to get the result
+        try:
+
+            result = await self._handler(request)
+
+        except Exception as err:
+            # Error in the handler
+            errer_text = "Error in request handler: " + str(err)
+            await send({"type": "http.response.start", "status": 500, "headers": []})
+            await send({"type": "http.response.body", "body": errer_text.encode()})
+            raise err
+
+        # === Process the handler output
+        try:
+
+            # Get status, headers and body from the result
+            if isinstance(result, tuple):
+                if len(result) == 3:
+                    status, headers, body = result
+                elif len(result) == 2 and isinstance(result[0], (set, dict)):
+                    # Also go this path on set to get a better error below
+                    status = 200
+                    headers, body = result
+                elif len(result) == 2:
+                    headers = {}
+                    status, body = result
+                elif len(result) == 1:
+                    status, headers, body = 200, {}, result[0]
+                else:
+                    raise ValueError(f"Handler returned {len(result)}-tuple.")
+            else:
+                status, headers, body = 200, {}, result
+
+            # Validate status and headers
+            assert isinstance(
+                status, int
+            ), f"Status code must be an int, not {type(status)}"
+            assert isinstance(
+                headers, dict
+            ), f"Headers must be a dict, not {type(headers)}"
+
+            # Convert the body
+            if isinstance(body, bytes):
+                pass
+            elif isinstance(body, str):
+                if body.startswith(("<!DOCTYPE ", "<html>")):
+                    headers.setdefault("content-type", "text/html")
+                else:
+                    headers.setdefault("content-type", "text/plain")
+                body = body.encode()
+            elif isinstance(body, dict):
+                try:
+                    body = json.dumps(body).encode()
+                except Exception as err:
+                    raise ValueError(f"Could not JSON encode body: {err}")
+                headers.setdefault("content-type", "application/json")
+            elif inspect.isasyncgen(body):
+                pass
+            else:
+                if inspect.isgenerator(body):
+                    raise ValueError(
+                        f"Body cannot be a regular generator, use an async generator."
+                    )
+                else:
+                    raise ValueError(f"Body cannot be {type(body)}.")
+
+            # Convert and further validate headers
+            if isinstance(body, bytes):
+                headers.setdefault("content-length", str(len(body)))
+            try:
+                rawheaders = [(k.encode(), v.encode()) for k, v in headers.items()]
+            except Exception:
+                raise ValueError("Header keys and values must all be strings.")
+
+        except Exception as err:
+            # Error in hanlding handler output
+            errer_text = "Error in processing handler output: " + str(err)
+            await send({"type": "http.response.start", "status": 500, "headers": []})
+            await send({"type": "http.response.body", "body": errer_text.encode()})
+            raise err
+
+        # === Send response
+        start = {"type": "http.response.start", "status": status, "headers": rawheaders}
+        if isinstance(body, bytes):
+            # The easy way; body as one message, not much error catching we can do here.
+            await send(start)
+            if isinstance(body, bytes):
+                await send({"type": "http.response.body", "body": body})
+        else:
+            # Chunked response, stuff can go wrong in the middle
+            start_is_sent = False
+            try:
+                async for chunk in body:
+                    if isinstance(chunk, str):
+                        chunk = chunk.encode()
+                    assert isinstance(
+                        chunk, bytes
+                    ), f"Body chunk must be str or bytes, not {type(chunk)}"
+                    if not start_is_sent:
+                        start_is_sent = True
+                        await send(start)
+                    await send(
+                        {"type": "http.response.body", "body": chunk, "more_body": True}
+                    )
+                await send(
+                    {"type": "http.response.body", "body": b"", "more_body": False}
+                )
+
+            except Exception as err:
+                if not start_is_sent:
+                    errer_text = "Error in chunked response: " + str(err)
+                    await send(
+                        {"type": "http.response.start", "status": 500, "headers": []}
+                    )
+                    await send(
+                        {"type": "http.response.body", "body": errer_text.encode()}
+                    )
+                else:  # end-of-body has also not been send
+                    await send(
+                        {"type": "http.response.body", "body": b"", "more_body": False}
+                    )
+                raise err
