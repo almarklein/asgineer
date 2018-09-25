21968872cd9faafaf9a1a2e8aff6178bb4aa5e76
Almar Klein <almar.klein@gmail.com>
2018-09-25 16:32:23 +0200

    add examples

---
+++ examples/example_request_info.py
@@ -0,0 +1,26 @@
+"""
+Simple asgish hanler to show information about the incoming request.
+"""
+
+from asgish import handler2asgi, run
+
+
+@handler2asgi
+async def main(request):
+
+    lines = [
+        "<!DOCTYPE html><html><meta><meta charset='UTF-8'></meta><body>",
+        f"<h2>request.method</h2>{request.method}",
+        f"<h2>request.url</h2>{request.url}",
+        f"<h2>request.path</h2>{request.path}",
+        f"<h2>request.querydict</h2>{request.querydict}",
+        f"<h2>request.headers</h2>",
+        "<br>".join(f"{key}: {val!r}" for key, val in request.headers.items()),
+        "<br>",
+        "</body></html>",
+    ]
+    return "<br>".join(lines)
+
+
+if __name__ == "__main__":
+    run(main, server="uvicorn", bind="localhost:80", log_level="info")
---
+++ examples/example_websocket.py
@@ -0,0 +1,67 @@
+"""
+Demonstrate websocket usage.
+"""
+
+from asgish import handler2asgi, run
+
+
+index = """
+<!DOCTYPE html>
+<html>
+<meta><meta charset='UTF-8'></meta>
+<body>
+Open console, and use "ws.send('x')" to send a message to the server.
+
+<script>
+
+window.onload = function() {
+    window.ws = new WebSocket('ws://' + window.location.host + '/ws');
+    window.ws.onmessage = function(m) {
+        console.log(m);
+    }
+    window.ws.onerror = function (e) {
+        console.log(e);
+    }
+    window.ws.onclose = function () {
+        console.log('ws closed');
+    }
+}
+
+</script>
+</body>
+</html>
+""".lstrip()
+
+
+@handler2asgi
+async def main(request):
+
+    if not request.path.rstrip("/"):
+        return index  # asgish sets the text/html content type
+    elif request.path.startswith("/ws"):
+        return await websocket_handler(request)
+    else:
+        return 404, {}, f"404 not found {request.path}"
+
+
+async def websocket_handler(request):
+    assert request.scope["type"] == "websocket", "Expected ws"
+    print("request", request)
+
+    await request.accept()  # todo: server part can do this?
+    await request.send("hello!")
+
+    async def waiter():
+        async for m in request.receive_iter():
+            print(m)
+        print("done")
+
+    import asyncio
+
+    await asyncio.create_task(waiter())
+    # The moment that we return, the websocket will be closed
+    # (if the ASGI server behaves correctly)
+
+
+if __name__ == "__main__":
+    run(main, server="uvicorn", bind="localhost:80", log_level="info")
