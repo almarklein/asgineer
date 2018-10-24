1932db5fe527e8e76da271924763e09604866cb4
Almar Klein <almar.klein@gmail.com>
2018-10-24 12:52:46 +0200

    add forgotten comitted test

---
+++ tests/test_run.py
@@ -0,0 +1,29 @@
+"""
+Test some specifics of the run function.
+"""
+
+import asgish
+import pytest
+
+
+async def handler(request):
+    return "ok"
+
+
+def test_run():
+
+    with pytest.raises(ValueError) as err:
+        asgish.run("foo", "nonexistingserver")
+    assert "full path" in str(err).lower()
+
+    with pytest.raises(ValueError) as err:
+        asgish.run("foo:bar", "nonexistingserver")
+    assert "invalid server" in str(err).lower()
+
+    with pytest.raises(ValueError) as err:
+        asgish.run(handler, "nonexistingserver")
+    assert "invalid server" in str(err).lower()
+
+
+if __name__ == "__main__":
+    test_run()
