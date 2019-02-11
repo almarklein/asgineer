243c7a255a8a0a64ed487544f79eda7281133e06
Almar Klein <almar.klein@gmail.com>
2019-02-11 23:04:42 +0100

    addd example to use with Starlette middleware

---
+++ examples/example_middleware.py
@@ -0,0 +1,23 @@
+"""
+Demonstrate the use of Starlette middleware to Asgish handlers.
+
+Because Asgish produces an ASGI-compatible application class, we can
+wrap it with ASGI middleware, e.g. from Starlette. Hooray for standards!
+"""
+
+import asgish
+
+from starlette.middleware.gzip import GZipMiddleware
+
+
+@asgish.to_asgi
+async def main(req):
+    return "hello world " * 1000
+
+
+# All requests that have a body over 1 KiB will be zipped
+main = GZipMiddleware(main, minimum_size=1024)
+
+
+if __name__ == "__main__":
+    asgish.run(f"__main__:main", 'uvicorn')
