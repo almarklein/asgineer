526bdc83d59acb901c7fe28a0a15354bc1e93456
Michael Lenaghan <michaell@dazzit.com>
2024-03-13 09:00:14 -0400

    Fix 304s and more. (#41)
    * Fix 304s and more.
    
    A quick summary:
    
     * Place double-quotes around ETag values.
     * Convert `str` bodies to `bytes`.
     * Always set the `content-length` header.
     * Return a 304 *after* `content-type` and `content-length` (and, maybe, `content-encoding`) headers have been set.
     * Don't set a "default" `content-type` header if the body is an empty byte string.
     * Don't set a "default" `content-length` header if the body is an empty byte string.
    
    * Don't send `content-encoding`, `content-length`, `content-type` with a 304 Not Modified response.
    
    Also: restructure code to simplify the flow.
    
    * Move the assignment to `body` up, and patch accordingly.
    
    * Change the decision making around adding default `content-length` and `content-type` headers.
    
    * Reformat using `black`.

--- asgineer/_app.py
+++ asgineer/_app.py
@@ -140,12 +140,25 @@ async def _handle_http(handler, request):
         where = "request handler"
         result = await handler(request)
 
+        def _response_has_body(
+            request, response_status, response_headers, response_body
+        ):
+            # https://www.rfc-editor.org/rfc/rfc7230#section-3.3
+            if request.method == "HEAD":
+                return False
+            if (100 <= response_status <= 199) or response_status in [204, 304]:
+                return False
+            return True
+
         if request._app_state == _request.CONNECTING:
             # Process the handler output
             where = "processing handler output"
             status, headers, body = normalize_response(result)
             # Make sure that there is a content type
-            if "content-type" not in headers:
+            if (
+                _response_has_body(request, status, headers, body)
+                and "content-type" not in headers
+            ):
                 headers["content-type"] = guess_content_type_from_body(body)
             # Convert the body
             if isinstance(body, bytes):
@@ -175,7 +188,11 @@ async def _handle_http(handler, request):
             # the content-length, the server sets Transfer-Encoding to chunked.
             if isinstance(body, bytes):
                 where = "sending response"
-                headers.setdefault("content-length", str(len(body)))
+                if (
+                    _response_has_body(request, status, headers, body)
+                    and "content-length" not in headers
+                ):
+                    headers["content-length"] = str(len(body))
                 await request.accept(status, headers)
                 await request.send(body, more=False)
             else:
