4c25e6adf1dafd8cb6038dffd4158822a81f1311
najtin <42641080+najtin@users.noreply.github.com>
2024-10-15 20:42:59 +0200

    format test for unixsocket using black

--- tests/test_unixsocket.py
+++ tests/test_unixsocket.py
@@ -1,5 +1,5 @@
 import subprocess
-import tempfile 
+import tempfile
 import socket
 import time
 import os
@@ -7,20 +7,20 @@ from pathlib import Path
 
 from common import get_backend
 
-HTTP_REQUEST = \
-    "GET / HTTP/1.1\r\n" + \
-    "Host: example.com\r\n" + \
-    "Connection: close\r\n\r\n"
+HTTP_REQUEST = (
+    "GET / HTTP/1.1\r\n" + "Host: example.com\r\n" + "Connection: close\r\n\r\n"
+)
 
 
 def test_unixsocket():
-        for backend_module in ["hypercorn", "uvicorn", "daphne"]:
-            with tempfile.TemporaryDirectory() as temp_folder:
-                socket_location = f"{temp_folder}/socket"
-                main_location = f"{temp_folder}/main.py"
-                project_location = Path(__file__).parent.parent.absolute()
-                print(project_location)
-                code_to_run = "\n".join([
+    for backend_module in ["hypercorn", "uvicorn", "daphne"]:
+        with tempfile.TemporaryDirectory() as temp_folder:
+            socket_location = f"{temp_folder}/socket"
+            main_location = f"{temp_folder}/main.py"
+            project_location = Path(__file__).parent.parent.absolute()
+            print(project_location)
+            code_to_run = "\n".join(
+                [
                     "# this allows us not to install asgineer and still import it",
                     "import importlib",
                     "import sys",
@@ -32,29 +32,27 @@ def test_unixsocket():
                     "import asgineer",
                     "@asgineer.to_asgi",
                     "async def main(request):",
-                    "    return \"Ok\"",
+                    '    return "Ok"',
                     "",
                     "if __name__ == '__main__':",
-                    f"    asgineer.run(main, '{backend_module}', 'unix:{socket_location}')"
-                ])
+                    f"    asgineer.run(main, '{backend_module}', 'unix:{socket_location}')",
+                ]
+            )
 
-                with open(main_location, "w") as file:
-                    file.write(code_to_run) 
+            with open(main_location, "w") as file:
+                file.write(code_to_run)
 
-                process = subprocess.Popen(
-                    ['python', main_location],
-                    cwd = project_location
-                )
+            process = subprocess.Popen(["python", main_location], cwd=project_location)
 
-                time.sleep(1)
+            time.sleep(1)
 
-                with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as client:
-                    client.connect(socket_location)
-                    client.send(HTTP_REQUEST.encode())
+            with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as client:
+                client.connect(socket_location)
+                client.send(HTTP_REQUEST.encode())
 
-                    response = client.recv(1024).decode()
-                
-                    if "200" not in response:
-                        raise RuntimeError("Unexpected response")
+                response = client.recv(1024).decode()
 
-                process.kill()
+                if "200" not in response:
+                    raise RuntimeError("Unexpected response")
+
+            process.kill()
