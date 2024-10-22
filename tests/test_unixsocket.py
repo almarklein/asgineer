3adc4e56471c11839617a5b4ce8cd123b0e2c4de
najtin <42641080+najtin@users.noreply.github.com>
2024-10-22 20:23:40 +0200

    retry initial failure and keep tempfolder

--- tests/test_unixsocket.py
+++ tests/test_unixsocket.py
@@ -2,6 +2,7 @@ import subprocess
 import tempfile
 import socket
 import time
+import shutil
 from pathlib import Path
 
 HTTP_REQUEST = (
@@ -11,44 +12,52 @@ HTTP_REQUEST = (
 
 def test_unixsocket():
     for backend_module in ["hypercorn", "uvicorn", "daphne"]:
-        with tempfile.TemporaryDirectory() as temp_folder:
-            socket_location = f"{temp_folder}/socket"
-            main_location = f"{temp_folder}/main.py"
-            project_location = Path(__file__).parent.parent.absolute()
-            code_to_run = "\n".join(
-                [
-                    "# this allows us not to install asgineer and still import it",
-                    "import importlib",
-                    "import sys",
-                    f"spec = importlib.util.spec_from_file_location('asgineer', '{project_location}/asgineer/__init__.py')",
-                    "module = importlib.util.module_from_spec(spec)",
-                    "sys.modules[spec.name] = module ",
-                    "spec.loader.exec_module(module)",
-                    "",
-                    "import asgineer",
-                    "@asgineer.to_asgi",
-                    "async def main(request):",
-                    '    return "Ok"',
-                    "",
-                    "if __name__ == '__main__':",
-                    f"    asgineer.run(main, '{backend_module}', 'unix:{socket_location}')",
-                ]
-            )
+        # mkdtemp instead of normal tempdir to prevent any issues that
+        # might occur with early clean up
+        temp_folder = tempfile.mkdtemp()
+        socket_location = f"{temp_folder}/socket"
+        main_location = f"{temp_folder}/main.py"
+        project_location = Path(__file__).parent.parent.absolute()
+        code_to_run = "\n".join(
+            [
+                "# this allows us not to install asgineer and still import it",
+                "import importlib",
+                "import sys",
+                f"spec = importlib.util.spec_from_file_location('asgineer', '{project_location}/asgineer/__init__.py')",
+                "module = importlib.util.module_from_spec(spec)",
+                "sys.modules[spec.name] = module ",
+                "spec.loader.exec_module(module)",
+                "",
+                "import asgineer",
+                "@asgineer.to_asgi",
+                "async def main(request):",
+                '    return "Ok"',
+                "",
+                "if __name__ == '__main__':",
+                f"    asgineer.run(main, '{backend_module}', 'unix:{socket_location}')",
+            ]
+        )
 
-            with open(main_location, "w") as file:
-                file.write(code_to_run)
+        with open(main_location, "w") as file:
+            file.write(code_to_run)
 
-            process = subprocess.Popen(["python", main_location], cwd=project_location)
+        process = subprocess.Popen(["python", main_location], cwd=project_location)
 
-            time.sleep(1)
+        with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as client:
+            max_tries = 3
+            for i in range(max_tries):
+                time.sleep(1)
+                try:
+                    client.connect(socket_location)
+                    client.send(HTTP_REQUEST.encode())
 
-            with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as client:
-                client.connect(socket_location)
-                client.send(HTTP_REQUEST.encode())
+                    response = client.recv(1024).decode()
 
-                response = client.recv(1024).decode()
+                    if "200" not in response:
+                        raise RuntimeError("Unexpected response")
+                except Exception as e:
+                    print(repr(e))
+                    print(f"Failed {i} times (max: {max_tries}), retrying...")
 
-                if "200" not in response:
-                    raise RuntimeError("Unexpected response")
-
-            process.kill()
+        process.kill()
+        shutil.rmtree(temp_folder)
